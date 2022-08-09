9d552fb131df346c6dc7757fef194b937af25d94
Paulo Roberto <paulinn.rs@gmail.com>
2022-08-09 14:48:46 -0300

    first commit

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,9 @@
+
+
+it('deve exibir a home page', function(){
+    cy.visit('https://training-wheels-qaninja.herokuapp.com/')
+    cy.title()
+        .should('eq' , 'Training Wheels | QAninja')
+    cy.get('ul[class=menu-list]')
+        .should('be.visible')
+})
