6612763cdcbbbb1a0b116948d9b36f5a439228a7
Paulo Roberto <104467309+paulinrs@users.noreply.github.com>
2022-08-14 15:46:39 -0300

    Mes

--- cypress/integration/datepicker.spec.js
+++ cypress/integration/datepicker.spec.js
@@ -4,7 +4,7 @@ it('selecionar nascimento do meu filho', function(){
     cy.visit('/datepicker')
 
     const date = {
-        month: 'set',
+        month: 'Sep',
         year: '2016',
         day: 5
     }
